1f9811e94c47d4066230722c116b05260efb390f
omprakash1907 <omprakashkjat19@gmail.com>
2025-05-13 17:38:19 +0530

    Update LearnMore component titles and introduction for clarity on Bitcoin topics

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -7,19 +7,19 @@ export default function LearnMore() {
   const articles = [
     {
       image: learnmore1,
-      title: "What is Lorem?",
+      title: "What is Bitcoin?",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       image: learnmore2,
-      title: "What is Ipsum?",
+      title: "What is Bitcoin Mining?",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       image: learnmore3,
-      title: "Lorem Ipsum",
+      title: "Bitcoin vs other investments",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
@@ -34,8 +34,8 @@ export default function LearnMore() {
           transition={{ duration: 0.5 }}
           viewport={{ once: true }}
         >
-          <h2 className="text-3xl font-bold mb-10">Learn more</h2>
-          <p className="text-gray-600 mb-10">Lorem Ipsum</p>
+          <h2 className="text-3xl font-bold mb-6 primary-font text-[#333333]">Learn About Bitcoin mining</h2>
+          <p className="text-gray-600 mb-10">An introduction to foundational Bitcoin concepts.</p>
         </motion.div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -55,7 +55,7 @@ export default function LearnMore() {
                   className="w-full h-full object-cover transition-transform hover:scale-105 duration-300"
                 />
               </div>
-              <h3 className="text-xl font-bold mb-2">{article.title}</h3>
+              <h3 className="text-xl font-bold mb-2 text-[#333333] py-2">{article.title}</h3>
               <p className="text-sm text-gray-600 mb-4">{article.description}</p>
             </motion.div>
           ))}
